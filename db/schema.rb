45c50b5d66a3d7273bac938531f31b7b4f1abc77
Martín Rosso <mrosso10@gmail.com>
2020-11-04 01:35:03 -0300

    Extensión unaccent de postgres

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2020_11_04_042938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "unaccent"
 
   create_table "admin_post_categories", force: :cascade do |t|
     t.string "nombre"
