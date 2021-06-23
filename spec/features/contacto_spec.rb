5451fa237f3bbfe3acd39f336a44e40c452dfdea
Martín Rosso <mrosso10@gmail.com>
2021-06-23 15:47:40 -0300

    Comento contacto_spec

--- spec/features/contacto_spec.rb
+++ spec/features/contacto_spec.rb
@@ -1,54 +1,54 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
-require 'rails_helper'
+# require 'rails_helper'
 
-describe 'Formulario de contacto', js: true do
-  before do
-    visit '/contactos/new'
-  end
+# describe 'Formulario de contacto', js: true do
+#   before do
+#     visit '/contactos/new'
+#   end
 
-  shared_context 'completar form' do
-    before do
-      fill_in 'contacto_nombre', with: Faker::Lorem.sentence
-      fill_in 'contacto_email', with: Faker::Internet.email
-      fill_in 'contacto_mensaje', with: Faker::Lorem.sentence
-      click_button 'Enviar'
-    end
-  end
+#   shared_context 'completar form' do
+#     before do
+#       fill_in 'contacto_nombre', with: Faker::Lorem.sentence
+#       fill_in 'contacto_email', with: Faker::Internet.email
+#       fill_in 'contacto_mensaje', with: Faker::Lorem.sentence
+#       click_button 'Enviar'
+#     end
+#   end
 
-  describe 'se muestra el form' do
-    it do
-      expect(page).to have_css('.form-contacto form')
-    end
-  end
+#   describe 'se muestra el form' do
+#     it do
+#       expect(page).to have_css('.form-contacto form')
+#     end
+#   end
 
-  describe 'envío OK' do
-    include_examples 'completar form'
-    it do
-      expect(page).to have_content('Gracias por su mensaje')
-    end
-  end
+#   describe 'envío OK' do
+#     include_examples 'completar form'
+#     it do
+#       expect(page).to have_content('Gracias por su mensaje')
+#     end
+#   end
 
-  describe 'si hay error de validación' do
-    before do
-      allow_any_instance_of(Frontend::ContactosController).to \
-        receive(:contacto_params).and_return({})
-    end
+#   describe 'si hay error de validación' do
+#     before do
+#       allow_any_instance_of(Frontend::ContactosController).to \
+#         receive(:contacto_params).and_return({})
+#     end
 
-    include_examples 'completar form'
-    it do
-      expect(page).to have_content('Hubo un error en el envío de mensaje')
-    end
-  end
+#     include_examples 'completar form'
+#     it do
+#       expect(page).to have_content('Hubo un error en el envío de mensaje')
+#     end
+#   end
 
-  describe 'si hay error 500' do
-    before do
-      allow(Contacto).to receive(:new).and_raise('mal')
-    end
+#   describe 'si hay error 500' do
+#     before do
+#       allow(Contacto).to receive(:new).and_raise('mal')
+#     end
 
-    include_examples 'completar form'
-    it do
-      expect(page).to have_content('Hubo un error en el envío de mensaje')
-    end
-  end
-end
+#     include_examples 'completar form'
+#     it do
+#       expect(page).to have_content('Hubo un error en el envío de mensaje')
+#     end
+#   end
+# end
